9c3424f4f187db813435c6d6476d434af2d4cf32
Umakajan Umakanthan <umakajan@umakajan.com>
2020-01-13 22:24:31 -0500

    use url package

--- index.js
+++ index.js
@@ -4,10 +4,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-  res.end('Hello world\n');
+  // Get URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
+  res.end('Hello World\n');
+
+  // Log the request
+  console.log(`Request received on path: ${trimmedPath}`);
 });
 
 // Start the server and listen on port 3000
